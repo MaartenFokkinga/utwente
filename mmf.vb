e6c4c9f4ac680a516e25e510f647fd48e74f0d7a
MaartenFokkinga <m.m.fokkinga@gmail.com>
2014-11-19 17:20:29 +0100

    First bulk commit

---
+++ mmf.vb
@@ -0,0 +1,17 @@
+<!-- =======================> SEE publ.vb instead! <================== -->
+<title>
+....
+</title>
+
+<h1>
+.......
+</h1>
+<a href="index.html">Maarten M Fokkinga</a>
+<hr>
+......
+<hr>
+The full paper (postscript version): <a HREF="mmf......ps">here</a>.
+<hr>
+<b>Bibtex data</b><pre>
+......
+</pre>
